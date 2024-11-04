ae52e7c2e7cf31fdb85e2e74f191f65df7fc66dc
Bhuvaneshwari <durgabhuvaneshwari4444@gmail.com>
2024-11-04 10:18:30 +0530

    included a .env file with the gemini-api key

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-
-import {assets} from './assets/assets'
-import Sidebar from './Components/Sidebar/Sidebar'
 import Main from './Main/Main'
 import "./index.css"
+import Sidebar from './Components/sidebar/Sidebar'
 const App = () => {
   return (
        <div className="app">
-          <Sidebar/>
-          <Main/>
+        <Sidebar/>
+        <Main/>
        </div>
   )
 }
